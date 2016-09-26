a61dbc596560b1d1de5f10e1695325e318df22f4
hzliukai <hzliukai@corp.netease.com>
2016-09-26 14:01:32 +0800

    fix sample

--- sample.go
+++ sample.go
@@ -8,11 +8,11 @@ import (
 	"github.com/NetEase-Object-Storage/nos-golang-sdk/nosclient"
 )
 
-func init() *nosclient.NosClient {
+func sample_init(endpoint, accessKey, secretKey string) *nosclient.NosClient {
 	conf := &config.Config{
-		Endpoint:  "<endpoint>",
-		AccessKey: "<AccessKeyId>",
-		SecretKey: "<SecretKey>",
+		Endpoint:  endpoint,
+		AccessKey: accessKey,
+		SecretKey: secretKey,
 
 		NosServiceConnectTimeout:    3,
 		NosServiceReadWriteTimeout:  60,
@@ -28,21 +28,37 @@ func init() *nosclient.NosClient {
 
 func main() {
 	path := "<File Path>"
-	objectRequest := &model.ObjectRequest{
+	endpoint := "<endpoint>"
+	accessKey := "<AccessKeyId>"
+	secretKey := "<SecretKey>"
+
+	nosClient := sample_init(endpoint, accessKey, secretKey)
+
+	putObjectRequest := &model.PutObjectRequest{
+		Bucket:   "<my-bucket>",
+		Object:   "<my-object>",
+		FilePath: path,
+	}
+	_, err := nosClient.PutObjectByFile(putObjectRequest)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	getObjectRequest := &model.GetObjectRequest{
 		Bucket: "<my-bucket>",
 		Object: "<my-object>",
 	}
-
-	nosClient := init()
-	nosClient.PutObjectByFile(path)
-
-	objectResult, err := nosClient.GetObject(objectRequest)
+	objectResult, err := nosClient.GetObject(getObjectRequest)
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
 		objectResult.Body.Close()
 	}
 
+	objectRequest := &model.ObjectRequest{
+		Bucket: "<my-bucket>",
+		Object: "<my-object>",
+	}
 	err = nosClient.DeleteObject(objectRequest)
 	if err != nil {
 		fmt.Println(err.Error())
